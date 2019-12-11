080937b55dce99090bc1464e2baf279381e5ea41
Anurag Pallaprolu <apallaprolu@ucsb.edu>
2019-12-11 07:58:21 -0800

    Removed getCrypto since that is being tracked in take2 branch

--- getCrypto.py
+++
@@ -1,6 +0,0 @@
-# We will use CoinCap API V2.0 to get the 10 minute interval data across different exchanges.
-
-import time
-
-def mtime2dt(mtime):
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(mtime))
