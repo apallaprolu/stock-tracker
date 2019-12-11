e7806e2dea097e9f9d97b43c9fe0be0bae2f285e
root <root@dhcp185-119.ece.ucsb.edu>
2019-12-11 11:57:02 -0800

    Seems to be some issue with CoinCap API. Commit before looking for alternatives.

--- getCrypto.py
+++ getCrypto.py
@@ -2,7 +2,7 @@
 
 import time
 import requests
-import ast
+import json
 from datetime import datetime
 
 
@@ -17,17 +17,32 @@ def dt2mtime(tstamp):
     dtobject = datetime.strptime(tstring, "%Y-%m-%d %H:%M:%S")
     return int(dtobject.timestamp()*1000)
 
-def getData(exchange, frequency, baseAsset, quoteAsset, startTime, endTime):
-    # We need start-time and end-time to be in the six-tuple format of (day, mon, year, hr, min, sec). 
-    url = "https://api.coincap.io/v2/candles?exchange="+str(exchange)+"&interval="+str(frequency)+"&baseId="+str(baseAsset)+"&quoteId="+str(quoteAsset)+"&start="+str(dt2mtime(startTime))+"&end="+str(dt2mtime(endTime))
-    headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
-    payload = {}
-    response = requests.request("GET", url, headers=headers, data = payload)
-    print("getData is processing request " + str(url))
-    return ast.literal_eval(response.text)
+def getData(exchanges, frequency, baseAsset, quoteAssets, startTime, endTime):
+    # We need start-time and end-time to be in the six-tuple format of (day, mon, year, hr, min, sec).
+    # Global response dictionary will be a sequential merge of individual responses. 
+    # Since MTIMEs are globally synchronous, it should not cause an issue.
+
+    resDict = {}
+
+    for exchange in exchanges:
+        for quoteAsset in quoteAssets:
+            url = "https://api.coincap.io/v2/candles?exchange="+str(exchange)+"&interval="+str(frequency)+"&baseId="+str(baseAsset)+"&quoteId="+str(quoteAsset)+"&start="+str(dt2mtime(startTime))+"&end="+str(dt2mtime(endTime))
+            headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+            payload = {}
+            response = requests.request("GET", url, headers=headers, data = payload)
+            print("getData is processing request " + str(url))
+            localDict = json.loads(response.text)
+            print(localDict)
+            try:
+                localDict["data."+str(quoteAsset)+"."+str(exchange)] = localDict["data"]
+                del localDict["data"]
+                localDict["timestamp."+str(quoteAsset)+"."+str(exchange)] = localDict["timestamp"]
+                del localDict["timestamp"]
+            except:
+                continue
+            resDict = {**resDict, **localDict}
+    return resDict
 
 
 def unifyQuoteDict(quoteDict):
-
-
-
+    pass
